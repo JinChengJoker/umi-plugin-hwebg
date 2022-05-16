6dddb1bf3371be4ba6333421be921589377ab99d
cheng <jinchengjoker@foxmail.com>
2022-05-16 15:43:52 +0800

    publish: 0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 // ref:
 // - https://umijs.org/plugins/api
 import { IApi } from '@umijs/types';
+import { traverseOpenApiList } from 'hwebg';
 
-export default function (api: IApi) {
-  api.logger.info('use plugin');
-
-  api.modifyHTML(($) => {
-    $('body').prepend(`<h1>hello umi plugin</h1>`);
-    return $;
+export default function(api: IApi) {
+  api.describe({
+    key: 'openAPI',
+    config: {
+      schema(joi) {
+        const itemSchema = joi.object({
+          schemaPath: joi.string(),
+          projectName: joi.string(),
+        });
+        return joi.alternatives(joi.array().items(itemSchema), itemSchema);
+      },
+    },
+    enableBy: api.EnableBy.config,
   });
 
+  api.registerCommand({
+    name: 'hwebg',
+    fn: async () => {
+      const openAPIConfig = api.config.openAPI;
+      if (Array.isArray(openAPIConfig)) {
+        traverseOpenApiList(openAPIConfig);
+        return;
+      }
+      traverseOpenApiList([openAPIConfig]);
+    },
+  });
 }
